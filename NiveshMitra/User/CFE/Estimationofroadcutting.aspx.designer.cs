1d5eaa9801096ee1d285a6f5c717358a456ac4d3
SumitDev775 <sumit_kanaujiya@cms.co.in>
2025-06-12 18:19:16 +0530

    commit populate data indutry details

--- NiveshMitra/User/CFE/Estimationofroadcutting.aspx.designer.cs
+++ NiveshMitra/User/CFE/Estimationofroadcutting.aspx.designer.cs
@@ -276,6 +276,69 @@ namespace NiveshMitra.User.CFE
         protected global::System.Web.UI.WebControls.TextBox TextBox2;
 
         /// <summary>
+        /// txtalondtheroad control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtalondtheroad;
+
+        /// <summary>
+        /// txtacrosstheroad control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtacrosstheroad;
+
+        /// <summary>
+        /// rbtnWetRiserYes control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rbtnWetRiserYes;
+
+        /// <summary>
+        /// rbtnWetRiserNo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rbtnWetRiserNo;
+
+        /// <summary>
+        /// RadioButton1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton RadioButton1;
+
+        /// <summary>
+        /// RadioButton2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton RadioButton2;
+
+        /// <summary>
+        /// DropDownList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+        /// <summary>
         /// btnClearAll control.
         /// </summary>
         /// <remarks>
